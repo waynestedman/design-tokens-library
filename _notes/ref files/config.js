48b0f9a7c1363acdf3d8ea565e36d9beb8b63774
Wayne Stedman <waynestedman@users.noreply.github.com>
2022-12-01 14:01:58 -0800

    build: some waiting updates; begin repo name change

---
+++ _notes/ref files/config.js
@@ -0,0 +1,60 @@
+const StyleDictionary = require("style-dictionary");
+const minifyDictionary = require('style-dictionary/lib/common/formatHelpers/minifyDictionary.js');
+const fileHeader = require('style-dictionary/lib/common/formatHelpers/fileHeader.js');
+
+StyleDictionary.registerFormat({
+  name: 'customJSFormat',
+  formatter: function({ dictionary, file }) {
+    return fileHeader({file}) +
+      'module.exports = ' +
+      JSON.stringify(minifyDictionary(dictionary.tokens), null, 2);
+      // module.exports['json/nested']({dictionary}) + ';';
+  }
+});
+
+module.exports = {
+  source: ["input/*.json"],
+  platforms: {
+    css: {
+      transformGroup: "css",
+      buildPath: "build/css/",
+      files: [
+        {
+          destination: "color.tokens.css",
+          format: "css/variables",
+        }
+      ]
+    },
+    scss: {
+      transformGroup: "scss",
+      buildPath: "build/scss/",
+      files: [
+        {
+          destination: "_color.tokens.scss",
+          format: "scss/variables",
+        }
+      ]
+    },
+    // json: {
+    //   // transformGroup: "json",
+    //   buildPath: "build/json/",
+    //   files: [
+    //     {
+    //       destination: "colors.json",
+    //       format: "json/nested", 
+    //     },
+    //   ],
+    // },
+    js: {
+      transformGroup: "js",
+      // transforms: ["color/hex8"],
+      buildPath: "build/js/",
+      files: [
+        {
+          destination: "colors.tokens.js",
+          format: "customJSFormat"
+        },
+      ]
+    }
+  },
+};
