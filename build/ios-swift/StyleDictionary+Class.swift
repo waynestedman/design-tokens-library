b70b5279b9a5cedb8c975fe20f92a7ef999db902
Wayne Stedman <waynestedman@users.noreply.github.com>
2022-12-01 14:44:03 -0800

    build: transformed & built new tokens

--- build/ios-swift/StyleDictionary+Class.swift
+++ build/ios-swift/StyleDictionary+Class.swift
@@ -4,29 +4,15 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 08 Mar 2022 18:39:56 GMT
+// Generated on Thu, 01 Dec 2022 22:41:21 GMT
 
 
 import UIKit
 
 public class StyleDictionaryClass {
-    public static let blue01 = #64b5f6
-    public static let blue02 = #9be7ff
-    public static let blue03 = #2286c3
-    public static let body = 16px
-    public static let body01 = [object Object]
+    public static let bold = 700
     public static let button = 14px
-    public static let button01 = [object Object]
     public static let caption = 12px
-    public static let caption01 = [object Object]
-    public static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
-    public static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    public static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
-    public static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    public static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    public static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    public static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
     public static let gray01 = #c7c7c7
     public static let gray02 = #eeeeee
     public static let gray03 = #212121
@@ -36,23 +22,20 @@ public class StyleDictionaryClass {
     public static let h4 = 26px
     public static let h5 = 22px
     public static let h6 = 18px
-    public static let heading1 = [object Object]
-    public static let heading2 = [object Object]
-    public static let heading3 = [object Object]
-    public static let heading4 = [object Object]
-    public static let heading5 = [object Object]
-    public static let heading6 = [object Object]
-    public static let montserrat = Montserrat
-    public static let purple01 = #870096
-    public static let raleway = Raleway
-    public static let red01 = #96000f
-    public static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
-    public static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
-    public static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
-    public static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
+    public static let indigo01 = #152a47
+    public static let indigo02 = #6c90e6
+    public static let light = 300
+    public static let montserrat = montserrat
+    public static let p = 16px
+    public static let radius01 = 8px
+    public static let radius02 = 50px
+    public static let radius03 = 18px
+    public static let raleway = raleway
+    public static let regular = 400
+    public static let semiBold = 500
     public static let subtitle = 14px
-    public static let subtitle01 = [object Object]
-    public static let teal01 = #009688
-    public static let teal02 = #52c7b8
-    public static let teal03 = #00675b
+    public static let teal01 = #00e6d6
+    public static let teal02 = #9dfffd
+    public static let teal03 = #00998f
+    public static let tokenSetOrder0 = global
 }
--- build/ios-swift/StyleDictionary+Struct.swift
+++ build/ios-swift/StyleDictionary+Struct.swift
@@ -4,29 +4,15 @@
 //
 
 // Do not edit directly
-// Generated on Tue, 08 Mar 2022 18:39:56 GMT
+// Generated on Thu, 01 Dec 2022 22:41:21 GMT
 
 
 import UIKit
 
 internal struct StyleDictionaryStruct {
-    internal static let blue01 = #64b5f6
-    internal static let blue02 = #9be7ff
-    internal static let blue03 = #2286c3
-    internal static let body = 16px
-    internal static let body01 = [object Object]
+    internal static let bold = 700
     internal static let button = 14px
-    internal static let button01 = [object Object]
     internal static let caption = 12px
-    internal static let caption01 = [object Object]
-    internal static let colorBaseGrayDark = UIColor(red: 0.067, green: 0.067, blue: 0.067, alpha: 1)
-    internal static let colorBaseGrayLight = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
-    internal static let colorBaseGrayMedium = UIColor(red: 0.600, green: 0.600, blue: 0.600, alpha: 1)
-    internal static let colorBaseGreen = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    internal static let colorBaseRed = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    internal static let colorFontBase = UIColor(red: 1.000, green: 0.000, blue: 0.000, alpha: 1)
-    internal static let colorFontSecondary = UIColor(red: 0.000, green: 1.000, blue: 0.000, alpha: 1)
-    internal static let colorFontTertiary = UIColor(red: 0.800, green: 0.800, blue: 0.800, alpha: 1)
     internal static let gray01 = #c7c7c7
     internal static let gray02 = #eeeeee
     internal static let gray03 = #212121
@@ -36,23 +22,20 @@ internal struct StyleDictionaryStruct {
     internal static let h4 = 26px
     internal static let h5 = 22px
     internal static let h6 = 18px
-    internal static let heading1 = [object Object]
-    internal static let heading2 = [object Object]
-    internal static let heading3 = [object Object]
-    internal static let heading4 = [object Object]
-    internal static let heading5 = [object Object]
-    internal static let heading6 = [object Object]
-    internal static let montserrat = Montserrat
-    internal static let purple01 = #870096
-    internal static let raleway = Raleway
-    internal static let red01 = #96000f
-    internal static let sizeFontBase = CGFloat(16.00) /* the base size of the font */
-    internal static let sizeFontLarge = CGFloat(32.00) /* the large size of the font */
-    internal static let sizeFontMedium = CGFloat(16.00) /* the medium size of the font */
-    internal static let sizeFontSmall = CGFloat(12.00) /* the small size of the font */
+    internal static let indigo01 = #152a47
+    internal static let indigo02 = #6c90e6
+    internal static let light = 300
+    internal static let montserrat = montserrat
+    internal static let p = 16px
+    internal static let radius01 = 8px
+    internal static let radius02 = 50px
+    internal static let radius03 = 18px
+    internal static let raleway = raleway
+    internal static let regular = 400
+    internal static let semiBold = 500
     internal static let subtitle = 14px
-    internal static let subtitle01 = [object Object]
-    internal static let teal01 = #009688
-    internal static let teal02 = #52c7b8
-    internal static let teal03 = #00675b
+    internal static let teal01 = #00e6d6
+    internal static let teal02 = #9dfffd
+    internal static let teal03 = #00998f
+    internal static let tokenSetOrder0 = global
 }
