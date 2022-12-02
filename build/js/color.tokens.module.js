19327d5904972e6a89594d52cf27fc9d05734edf
Wayne Stedman <waynestedman@users.noreply.github.com>
2022-12-01 16:45:33 -0800

    build: updated tokens; transformed & built

---
+++ build/js/color.tokens.module.js
@@ -0,0 +1,675 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 02 Dec 2022 00:30:14 GMT
+ */
+
+module.exports = {
+  "radius01": {
+    "value": "8px",
+    "type": "borderRadius",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "8px",
+      "type": "borderRadius"
+    },
+    "name": "radius01",
+    "attributes": {},
+    "path": [
+      "radius01"
+    ]
+  },
+  "radius02": {
+    "value": "50px",
+    "type": "borderRadius",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "50px",
+      "type": "borderRadius"
+    },
+    "name": "radius02",
+    "attributes": {},
+    "path": [
+      "radius02"
+    ]
+  },
+  "radius03": {
+    "value": "18px",
+    "type": "borderRadius",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "18px",
+      "type": "borderRadius"
+    },
+    "name": "radius03",
+    "attributes": {},
+    "path": [
+      "radius03"
+    ]
+  },
+  "teal01": {
+    "value": "#00e6d6",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#00e6d6",
+      "type": "color"
+    },
+    "name": "teal01",
+    "attributes": {},
+    "path": [
+      "teal01"
+    ]
+  },
+  "teal02": {
+    "value": "#9dfffd",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#9dfffd",
+      "type": "color"
+    },
+    "name": "teal02",
+    "attributes": {},
+    "path": [
+      "teal02"
+    ]
+  },
+  "teal03": {
+    "value": "#00998f",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#00998f",
+      "type": "color"
+    },
+    "name": "teal03",
+    "attributes": {},
+    "path": [
+      "teal03"
+    ]
+  },
+  "indigo01": {
+    "value": "#152a47",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#152a47",
+      "type": "color"
+    },
+    "name": "indigo01",
+    "attributes": {},
+    "path": [
+      "indigo01"
+    ]
+  },
+  "indigo02": {
+    "value": "#6c90e6",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#6c90e6",
+      "type": "color"
+    },
+    "name": "indigo02",
+    "attributes": {},
+    "path": [
+      "indigo02"
+    ]
+  },
+  "gray01": {
+    "value": "#c7c7c7",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#c7c7c7",
+      "type": "color"
+    },
+    "name": "gray01",
+    "attributes": {},
+    "path": [
+      "gray01"
+    ]
+  },
+  "gray02": {
+    "value": "#eeeeee",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#eeeeee",
+      "type": "color"
+    },
+    "name": "gray02",
+    "attributes": {},
+    "path": [
+      "gray02"
+    ]
+  },
+  "gray03": {
+    "value": "#212121",
+    "type": "color",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "#212121",
+      "type": "color"
+    },
+    "name": "gray03",
+    "attributes": {},
+    "path": [
+      "gray03"
+    ]
+  },
+  "Raleway": {
+    "value": "Raleway",
+    "type": "fontFamilies",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "Raleway",
+      "type": "fontFamilies"
+    },
+    "name": "Raleway",
+    "attributes": {},
+    "path": [
+      "Raleway"
+    ]
+  },
+  "Montserrat": {
+    "value": "Montserrat",
+    "type": "fontFamilies",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "Montserrat",
+      "type": "fontFamilies"
+    },
+    "name": "Montserrat",
+    "attributes": {},
+    "path": [
+      "Montserrat"
+    ]
+  },
+  "light": {
+    "value": 300,
+    "type": "fontWeights",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": 300,
+      "type": "fontWeights"
+    },
+    "name": "light",
+    "attributes": {},
+    "path": [
+      "light"
+    ]
+  },
+  "regular": {
+    "value": 400,
+    "type": "fontWeights",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": 400,
+      "type": "fontWeights"
+    },
+    "name": "regular",
+    "attributes": {},
+    "path": [
+      "regular"
+    ]
+  },
+  "medium": {
+    "value": 500,
+    "type": "fontWeights",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": 500,
+      "type": "fontWeights"
+    },
+    "name": "medium",
+    "attributes": {},
+    "path": [
+      "medium"
+    ]
+  },
+  "semibold": {
+    "value": 600,
+    "type": "fontWeights",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": 600,
+      "type": "fontWeights"
+    },
+    "name": "semibold",
+    "attributes": {},
+    "path": [
+      "semibold"
+    ]
+  },
+  "bold": {
+    "value": 700,
+    "type": "fontWeights",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": 700,
+      "type": "fontWeights"
+    },
+    "name": "bold",
+    "attributes": {},
+    "path": [
+      "bold"
+    ]
+  },
+  "h1": {
+    "value": "38px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "38px",
+      "type": "fontSizes"
+    },
+    "name": "h1",
+    "attributes": {},
+    "path": [
+      "h1"
+    ]
+  },
+  "h2": {
+    "value": "34px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "34px",
+      "type": "fontSizes"
+    },
+    "name": "h2",
+    "attributes": {},
+    "path": [
+      "h2"
+    ]
+  },
+  "h3": {
+    "value": "30px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "30px",
+      "type": "fontSizes"
+    },
+    "name": "h3",
+    "attributes": {},
+    "path": [
+      "h3"
+    ]
+  },
+  "h4": {
+    "value": "26px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "26px",
+      "type": "fontSizes"
+    },
+    "name": "h4",
+    "attributes": {},
+    "path": [
+      "h4"
+    ]
+  },
+  "h5": {
+    "value": "22px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "22px",
+      "type": "fontSizes"
+    },
+    "name": "h5",
+    "attributes": {},
+    "path": [
+      "h5"
+    ]
+  },
+  "h6": {
+    "value": "18px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "18px",
+      "type": "fontSizes"
+    },
+    "name": "h6",
+    "attributes": {},
+    "path": [
+      "h6"
+    ]
+  },
+  "subtitle": {
+    "value": "14px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "14px",
+      "type": "fontSizes"
+    },
+    "name": "subtitle",
+    "attributes": {},
+    "path": [
+      "subtitle"
+    ]
+  },
+  "p": {
+    "value": "16px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "16px",
+      "type": "fontSizes"
+    },
+    "name": "p",
+    "attributes": {},
+    "path": [
+      "p"
+    ]
+  },
+  "button": {
+    "value": "14px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "14px",
+      "type": "fontSizes"
+    },
+    "name": "button",
+    "attributes": {},
+    "path": [
+      "button"
+    ]
+  },
+  "caption": {
+    "value": "12px",
+    "type": "fontSizes",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": "12px",
+      "type": "fontSizes"
+    },
+    "name": "caption",
+    "attributes": {},
+    "path": [
+      "caption"
+    ]
+  },
+  "heading1": {
+    "value": {
+      "fontFamily": "Raleway",
+      "fontWeight": 700,
+      "fontSize": "38px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Raleway",
+        "fontWeight": 700,
+        "fontSize": "38px"
+      },
+      "type": "typography"
+    },
+    "name": "heading1",
+    "attributes": {},
+    "path": [
+      "heading1"
+    ]
+  },
+  "heading2": {
+    "value": {
+      "fontFamily": "Raleway",
+      "fontWeight": 600,
+      "fontSize": "34px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Raleway",
+        "fontWeight": 600,
+        "fontSize": "34px"
+      },
+      "type": "typography"
+    },
+    "name": "heading2",
+    "attributes": {},
+    "path": [
+      "heading2"
+    ]
+  },
+  "heading3": {
+    "value": {
+      "fontFamily": "Raleway",
+      "fontSize": "30px",
+      "fontWeight": 500
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Raleway",
+        "fontSize": "30px",
+        "fontWeight": 500
+      },
+      "type": "typography"
+    },
+    "name": "heading3",
+    "attributes": {},
+    "path": [
+      "heading3"
+    ]
+  },
+  "heading4": {
+    "value": {
+      "fontFamily": "Raleway",
+      "fontWeight": 500,
+      "fontSize": "26px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Raleway",
+        "fontWeight": 500,
+        "fontSize": "26px"
+      },
+      "type": "typography"
+    },
+    "name": "heading4",
+    "attributes": {},
+    "path": [
+      "heading4"
+    ]
+  },
+  "heading5": {
+    "value": {
+      "fontFamily": "Raleway",
+      "fontWeight": 400,
+      "fontSize": "22px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Raleway",
+        "fontWeight": 400,
+        "fontSize": "22px"
+      },
+      "type": "typography"
+    },
+    "name": "heading5",
+    "attributes": {},
+    "path": [
+      "heading5"
+    ]
+  },
+  "heading6": {
+    "value": {
+      "fontFamily": "Raleway",
+      "fontWeight": 400,
+      "fontSize": "18px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Raleway",
+        "fontWeight": 400,
+        "fontSize": "18px"
+      },
+      "type": "typography"
+    },
+    "name": "heading6",
+    "attributes": {},
+    "path": [
+      "heading6"
+    ]
+  },
+  "subheading": {
+    "value": {
+      "fontFamily": "Raleway",
+      "fontWeight": 400,
+      "fontSize": "14px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Raleway",
+        "fontWeight": 400,
+        "fontSize": "14px"
+      },
+      "type": "typography"
+    },
+    "name": "subheading",
+    "attributes": {},
+    "path": [
+      "subheading"
+    ]
+  },
+  "body": {
+    "value": {
+      "fontFamily": "Montserrat",
+      "fontWeight": 400,
+      "fontSize": "16px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Montserrat",
+        "fontWeight": 400,
+        "fontSize": "16px"
+      },
+      "type": "typography"
+    },
+    "name": "body",
+    "attributes": {},
+    "path": [
+      "body"
+    ]
+  },
+  "note": {
+    "value": {
+      "fontFamily": "Montserrat",
+      "fontWeight": 400,
+      "fontSize": "12px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Montserrat",
+        "fontWeight": 400,
+        "fontSize": "12px"
+      },
+      "type": "typography"
+    },
+    "name": "note",
+    "attributes": {},
+    "path": [
+      "note"
+    ]
+  },
+  "buttonText": {
+    "value": {
+      "fontFamily": "Montserrat",
+      "fontWeight": 400,
+      "fontSize": "14px"
+    },
+    "type": "typography",
+    "filePath": "tokens/transformed-tokens.json",
+    "isSource": true,
+    "original": {
+      "value": {
+        "fontFamily": "Montserrat",
+        "fontWeight": 400,
+        "fontSize": "14px"
+      },
+      "type": "typography"
+    },
+    "name": "buttonText",
+    "attributes": {},
+    "path": [
+      "buttonText"
+    ]
+  },
+  "tokenSetOrder": {
+    "0": {
+      "value": "global",
+      "type": "other",
+      "filePath": "tokens/transformed-tokens.json",
+      "isSource": true,
+      "original": {
+        "value": "global",
+        "type": "other"
+      },
+      "name": "0",
+      "attributes": {},
+      "path": [
+        "tokenSetOrder",
+        "0"
+      ]
+    }
+  }
+};
