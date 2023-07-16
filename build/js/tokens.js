7febd2dfa98063007131e2ff5ed1dcbb040f5088
Wayne Stedman <waynestedman@users.noreply.github.com>
2023-07-16 15:04:54 -0700

    re-build

---
+++ build/js/tokens.js
@@ -0,0 +1,150 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 16 Jul 2023 22:03:57 GMT
+ */
+
+module.exports = {
+  "emerald-green": "#05878a",
+  "sapphire-blue": "#074e67",
+  "purple-jewel": "#5a175d",
+  "wine-red": "#67074e",
+  "gold-yellow": "#dd9933",
+  "white": "#ffffff",
+  "black": "#000000",
+  "gray-50": "#808080",
+  "rm3-lt-gray": "#d4d3cf",
+  "rm3-md-gray": "#70716a",
+  "rm3-dk-green": "#242721",
+  "rm3-primary-green": "#59817b",
+  "rm3-focal-purple": "#581a54",
+  "rm3-beige": "#f6f1e3",
+  "rm1-mid-gray": "#a7a7a7",
+  "rm1-light-gray": "#d2d2d2",
+  "rm1-primary-blue": "#1e3a61",
+  "rm1-accent-orange": "#c34a39",
+  "rm1-secondary-cream": "#d8b8a1",
+  "rm1-secondary-brown": "#3b2e26",
+  "fontFamilies": {
+    "raleway": "Raleway",
+    "montserrat": "Montserrat"
+  },
+  "lineHeights": {
+    "0": "AUTO"
+  },
+  "fontWeights": {
+    "raleway-0": "Bold",
+    "raleway-1": "SemiBold",
+    "montserrat-2": "Regular",
+    "raleway-3": "Medium",
+    "raleway-4": "Regular",
+    "montserrat-5": "Light"
+  },
+  "fontSize": {
+    "14": 14,
+    "16": 16,
+    "21": 21.33,
+    "28": 28.43,
+    "37": 37.9,
+    "50": 50.52,
+    "67": 67.34
+  },
+  "letterSpacing": {
+    "0": "0%"
+  },
+  "paragraphSpacing": {
+    "0": 0
+  },
+  "Main-title": {
+    "fontFamily": "Raleway",
+    "fontWeight": "Bold",
+    "lineHeight": "AUTO",
+    "fontSize": 67.34,
+    "letterSpacing": "0%",
+    "paragraphSpacing": 0,
+    "paragraphIndent": "0px",
+    "textCase": "none",
+    "textDecoration": "none"
+  },
+  "secondary-title": {
+    "fontFamily": "Raleway",
+    "fontWeight": "SemiBold",
+    "lineHeight": "AUTO",
+    "fontSize": 50.52,
+    "letterSpacing": "0%",
+    "paragraphSpacing": 0,
+    "paragraphIndent": "0px",
+    "textCase": "none",
+    "textDecoration": "none"
+  },
+  "body": {
+    "fontFamily": "Montserrat",
+    "fontWeight": "Regular",
+    "lineHeight": "AUTO",
+    "fontSize": 16,
+    "letterSpacing": "0%",
+    "paragraphSpacing": 0,
+    "paragraphIndent": "0px",
+    "textCase": "none",
+    "textDecoration": "none"
+  },
+  "heading3": {
+    "fontFamily": "Raleway",
+    "fontWeight": "Medium",
+    "lineHeight": "AUTO",
+    "fontSize": 37.9,
+    "letterSpacing": "0%",
+    "paragraphSpacing": 0,
+    "paragraphIndent": "0px",
+    "textCase": "none",
+    "textDecoration": "none"
+  },
+  "heading4": {
+    "fontFamily": "Raleway",
+    "fontWeight": "Medium",
+    "fontSize": 28.43
+  },
+  "heading6": {
+    "fontFamily": "Raleway",
+    "fontWeight": "Medium",
+    "fontSize": 16
+  },
+  "heading5": {
+    "fontFamily": "Raleway",
+    "fontWeight": "Medium",
+    "fontSize": 21.33
+  },
+  "eyebrow": {
+    "fontFamily": "Raleway",
+    "fontWeight": "Medium",
+    "lineHeight": "AUTO",
+    "fontSize": 14,
+    "letterSpacing": "0%",
+    "paragraphSpacing": 0,
+    "paragraphIndent": "0px",
+    "textCase": "none",
+    "textDecoration": "none"
+  },
+  "caption": {
+    "fontFamily": "Montserrat",
+    "fontWeight": "Light",
+    "lineHeight": "AUTO",
+    "fontSize": 14,
+    "letterSpacing": "0%",
+    "paragraphSpacing": 0,
+    "paragraphIndent": "0px",
+    "textCase": "none",
+    "textDecoration": "none"
+  },
+  "textCase": {
+    "none": "none"
+  },
+  "textDecoration": {
+    "none": "none"
+  },
+  "paragraphIndent": {
+    "0": "0px"
+  },
+  "tokenSetOrder": {
+    "0": "deep-dive"
+  }
+}
